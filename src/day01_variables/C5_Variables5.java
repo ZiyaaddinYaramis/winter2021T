bda7f20a92b3e8c09802c9b17cd0844481347b52
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2022-04-06 22:43:05 +0300

    guncelleme

--- src/day01_variables/C5_Variables5.java
+++ src/day01_variables/C5_Variables5.java
@@ -10,7 +10,7 @@ public class C5_Variables5 {
 		
 		System.out.println(sayi1/sayi2); // 10/4=2 ikisi de tam sayi oldugu icin sonucu tam sayi verir
 		
-		System.out.println(sayi1/sayi3); // 10/4=2.5 sayi3 double oldugu icin sonucuda virgullu verir  +
+		System.out.println(sayi1/sayi3); // 10/4=2.5 sayi3 double oldugu icin sonucuda virgullu verir
 		
 
 	}
